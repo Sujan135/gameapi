e8d6ef1145d45fc3155be8d33106773d55883b2e
Sujan Banjara <banjara.suzan12@gmail.com>
2025-06-16 09:59:54 -0500

    Add GameRepository to manage Game entities

--- gameapi/src/main/java/com/sujan/gameapi/repository/GameRepository.java
+++ gameapi/src/main/java/com/sujan/gameapi/repository/GameRepository.java
@@ -3,5 +3,5 @@ package com.sujan.gameapi.repository;
 import com.sujan.gameapi.model.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GameRepository extends JpaRepository<Game, Long> {
+public interface GameRepository Add GameRepository to manage Game entities extends JpaRepository<Game, Long> {
 }
