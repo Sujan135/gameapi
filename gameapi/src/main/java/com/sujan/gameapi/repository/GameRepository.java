22ee986e2771cd27f741b0d6d049535da8be65c9
Sujan Banjara <banjara.suzan12@gmail.com>
2025-06-17 10:07:58 -0500

    Add custom repository method to search games by title or genre

--- gameapi/src/main/java/com/sujan/gameapi/repository/GameRepository.java
+++ gameapi/src/main/java/com/sujan/gameapi/repository/GameRepository.java
@@ -3,5 +3,9 @@ package com.sujan.gameapi.repository;
 import com.sujan.gameapi.model.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GameRepository  extends JpaRepository<Game, Long> {
+    List<Game> findByTitleContainingIgnoreCaseOrGenreContainingIgnoreCase(String title, String genre);
+
 }
